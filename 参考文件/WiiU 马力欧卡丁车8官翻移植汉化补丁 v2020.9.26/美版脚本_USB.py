532f228de84257c90068d3d55446ec508d1a80bb
Darth-Koopa <45894930+Darth-Koopa@users.noreply.github.com>
2025-05-10 18:44:20 -0400

    Add files via upload

---
+++ 参考文件/WiiU 马力欧卡丁车8官翻移植汉化补丁 v2020.9.26/美版脚本_USB.py
@@ -0,0 +1,25 @@
+import wupclient
+
+if __name__ == '__main__':
+    print("Connecting to wupserver")
+    w = wupclient.wupclient()
+    if(w.s == None):
+        print("Failed to connect to wupserver!")
+        exit()
+    print("Connected!")
+    wupclient.w = w
+    print("Mounting SD")
+    ret = wupclient.mount_sd()
+    if(ret == 0):
+        print("Failed to mount SD!")
+    else:
+        print("Mounted! Copying content file from SD")
+        ret = w.cpdir("/vol/storage_sdcard/haxchi/mariokart8gfyzhhbd", "/vol/storage_usb01/usr/title/0005000e/1010ec00")
+    import os 
+    os.system('pause')
+    if(w.fsa_handle != None):
+        w.close(w.fsa_handle)
+        w.fsa_handle = None
+    if(w.s != None):
+        w.s.close()
+        w.s = None
